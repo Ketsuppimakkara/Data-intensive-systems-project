60f9d2069fcc535c4c8678871ac0b53f316e6c50
Ketsuppimakkara <lassi.laukkarinen@gmail.com>
2023-12-04 00:37:46 +0200

    Implemented in-road sensor database using postgres4

---
+++ inroad_database.js
@@ -0,0 +1,18 @@
+const {Pool} = require('pg');
+
+//Make sure you have postgres server running on localhost port number 5432. If you have set different usernames and passwords, change them here.
+
+const pool = new Pool({
+    user: 'postgres',
+    password: 'admin1234',
+    host: 'localhost',
+    port: "5432",
+    database: 'inroad'
+});
+
+pool.on('error',(err,client) =>{
+    console.error('Unexpected error on idle client', err)
+    process.exit(-1);
+})
+
+module.exports = pool;
