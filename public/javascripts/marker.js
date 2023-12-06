51979dd5b80eea1747f8081e8683e9297feb1018
Ketsuppimakkara <lassi.laukkarinen@gmail.com>
2023-12-06 16:46:56 +0200

    Added get by ID function to filter map

--- public/javascripts/marker.js
+++ public/javascripts/marker.js
@@ -13,6 +13,19 @@ class observation{
 var observations = [];
 var markers = [];
 
+async function getbyId(){
+    ids = document.getElementById("idSearch").value.split(',')
+    for(i = 0; i<markers.length; i++){
+        hideMarker(markers[i].marker)
+        for(j = 0; j<ids.length; j++){
+            if(markers[i].id==ids[j]){
+                showMarker(markers[i].marker)
+            }
+        }
+    }
+}
+
+
 
 async function loadData(sensorType){
     var response;
@@ -43,30 +56,39 @@ async function loadData(sensorType){
             popuptext = popuptext + "<br>"+`${key}: ${value}`;
         }
         marker.bindPopup(popuptext);
-        markers.push(new observation(observations[i].id,observations[i].sensorId,sensorType,observations[i].latitude,observations[i].longitude,observations[i].timestamp,marker))
+        markers.push(new observation(observations[i].observationid,observations[i].sensorId,sensorType,observations[i].latitude,observations[i].longitude,observations[i].timestamp,marker))
         i++
     }
+}
+
+function showMarker(marker){
+    marker._icon.style.display=''
+    marker._shadow.style.display=''
+}
+
+function hideMarker(marker){
+    if(marker._popup._isOpen){
+        marker.closePopup()
+    }
+    marker._icon.style.display='none'
+    marker._shadow.style.display='none'
+}
+
+function toggleMarkerDisplay(marker){
+    if(marker._icon.style.display != 'none' | marker._shadow.style.display != 'none'){
+        hideMarker(marker)
+    }
+    else{
+        showMarker(marker)
+    } 
 }
 
 function toggleMarkers(sensorType){
     for(i = 0; i<markers.length; i++){
         if(markers[i].sensorType === sensorType){
-            if(markers[i].marker._popup._isOpen){
-                markers[i].marker.closePopup()
-            }
-
-            if(markers[i].marker._icon.style.display != 'none' | markers[i].marker._shadow.style.display != 'none'){
-                markers[i].marker._icon.style.display='none'
-                markers[i].marker._shadow.style.display='none'
-            }
-            else{
-                markers[i].marker._icon.style.display=''
-                markers[i].marker._shadow.style.display=''
-            }
-
+            toggleMarkerDisplay(markers[i].marker)
         }
     }
-
 }
 
 function clearMarkers(sensorType){
